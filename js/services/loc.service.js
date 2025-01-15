1bfa79221589f5216d68a2cebba71b1dcd21be71
Dor-J <Mr.DorJosef@gmail.com>
2025-01-15 16:48:04 +0200

    Remove space

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -54,7 +54,7 @@ function query() {
     } else if (gSortBy.name !== undefined) {
       locs.sort((p1, p2) => p1.name.localeCompare(p2.name) * gSortBy.name)
     } else if (gSortBy.date !== undefined) {
-        locs.sort((p1, p2) => (p2.createdAt - p1.createdAt) * gSortBy.date)
+      locs.sort((p1, p2) => (p2.createdAt - p1.createdAt) * gSortBy.date)
     }
 
     return locs
